9fdcb5fd1542f619d82aa75ec62e50ef5d0f0d15
HarshSinha17 <75541881+HarshSinha17@users.noreply.github.com>
2021-01-06 20:05:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+var inp = document.getElementById("inp");
+function put(digit){
+  inp.value+=digit;
+}
+function putdot(){
+  inp.value="";
+}
+function clear(){
+  inp.value=".";
+}
+function putoperplus(){
+  inp.value+="+";
+  }
+function putoperminus(){
+  inp.value+="-";
+  }
+  function putoperdiv(){
+    inp.value+="/";
+    }
+    function putoperinto(){
+      inp.value+="*";
+      }
+function calculate(){
+  var res = eval(inp.value);
+  inp.value=res;
+}
