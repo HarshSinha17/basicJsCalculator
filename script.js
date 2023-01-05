d1e9ea8bee3be8f454ef8ab86f4250034eff19a9
Harsh Sinha <hsinha2006@gmail.com>
2023-01-05 14:35:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 
-var inp = document.getElementById("inp");
-function put(digit){
-  inp.value+=digit;
+var inp = document.getElementById(
+ "inp");
+
+function put(digit) {
+ inp.value += digit;
 }
-function putdot(){
-  inp.value="";
+
+function putdot() {
+ inp.value = "";
 }
-function clear(){
-  inp.value=".";
+
+function clear() {
+ inp.value = ".";
 }
-function putoperplus(){
-  inp.value+="+";
-  }
-function putoperminus(){
-  inp.value+="-";
-  }
-  function putoperdiv(){
-    inp.value+="/";
-    }
-    function putoperinto(){
-      inp.value+="*";
-      }
-function calculate(){
-  var res = eval(inp.value);
-  inp.value=res;
+
+function putoperplus() {
+ inp.value += "+";
+}
+
+function putoperminus() {
+ inp.value += "-";
+}
+
+function putoperdiv() {
+ inp.value += "/";
+}
+
+function putoperinto() {
+ inp.value += "*";
+}
+
+function calculate() {
+ var res = eval(inp.value);
+ inp.value = res;
 }
